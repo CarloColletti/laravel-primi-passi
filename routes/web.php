d14302a76fcdf4a862ff91416c1895feb2802d6e
CarloColletti~ <carlocolletti5@gmail.com>
2023-03-28 23:05:54 +0200

    Bonus Add

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,24 @@ Route::get('/', function () {
 
     return view('home', compact('title'));
 })->name('home');
+
+Route::get('/about', function () {
+
+    $title = "Hello Words";
+
+    return view('about', compact('title'));
+})->name('about');
+
+Route::get('/shop', function () {
+
+    $title = "Hello Wordss";
+
+    return view('shop', compact('title'));
+})->name('shop');
+
+Route::get('/contacts', function () {
+
+    $title = "Hello Wordsss";
+
+    return view('contacts', compact('title'));
+})->name('contacts');
